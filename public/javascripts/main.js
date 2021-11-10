661371b84d116d305c6418ec78cedf90ad4c0b67
INMD1 <lyw514549@gmail.com>
2021-11-10 16:02:39 +0900

    update

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,16 +1,24 @@
 const osu = require('node-os-utils');
+const os = require('os');
 const fs = require('fs')
+const cpus = os.cpus()
 const cpu = osu.cpu;
 const mem = osu.mem;;
+const netstat = osu.netstat
+
 //3초마다  정보를 수정함
 re = setInterval(async function () {
     const acpu = await cpu.usage() 
     const aram = await mem.info()
+    const anetstats = await netstat.inOut()
     let json = {
         "cpu": {
-            "temp" : acpu
+            "model" : cpus[0].model,
+            "usage" : acpu
         },
-        "ram": aram
+        "ram": aram,
+        "netstats" : anetstats
     }
     fs.writeFileSync('/home/ubuntu/바탕화면/github/computerapi/public/json/test.json', JSON.stringify(json), 'utf8');
 }, 3000);
+
