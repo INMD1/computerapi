bca89b13b137e00f7247e869fd566477abf1ac5e
INMD1 <lyw514549@gmail.com>
2021-12-29 07:22:20 +0900

    path fix

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,15 @@ const express = require('express');
 const router = express.Router();
 const fs = require('fs');
 const path = require('path');
-const pathfile = path.dirname(__filename).replace("javascripts", "");
+const pathfile = path.dirname(__filename).replace("routes", "");
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
 router.get('/test', function(req, res) {
-  res.status(200).json(JSON.parse(fs.readFileSync(`${pathfile}/json/data.json`, 'utf8')));
+  res.status(200).json(JSON.parse(fs.readFileSync(`${pathfile}/public/json/data.json`, 'utf8')));
 });
+
 
 module.exports = router;
