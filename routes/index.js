b6d8af13b7fdd3ee99d40cc95c6eed78f9288b62
INMD1 <lyw514549@gmail.com>
2022-01-16 20:24:32 +0900

    CORS 해결

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,12 @@ const router = express.Router();
 const fs = require('fs');
 const path = require('path');
 const pathfile = path.dirname(__filename).replace("routes", "");
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
 
-router.get('/test', function(req, res) {
+const cors = require('cors');
+
+router.use(cors());
+/* GET home page. */
+router.get('/', function(req, res) {
   res.status(200).json(JSON.parse(fs.readFileSync(`${pathfile}/public/json/data.json`, 'utf8')));
 });
 
