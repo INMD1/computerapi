ae2afa14ece0f1465285a42b88e7a1927c5d29e5
INMD1 <lyw514549@gmail.com>
2022-02-08 10:42:26 +0900

    node ver error fix

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 const {
     snapshot
-} = require("process-list");
+} = require("@senfo/process-list");
 const cpuStat = require('cpu-stat');
 
 //선언
